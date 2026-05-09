008ca61883500719dd7c59ebdfeaefb8881fdea2
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-09 21:09:30 +0100

    change home page styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full',
+  },
+  {
+    path: 'movies',
+    loadComponent: () =>
+      import('./features/movies/pages/movie-list/movie-list').then((m) => m.MovieList),
+  },
+  {
+    path: 'movies/:id',
+    loadComponent: () =>
+      import('./features/movie-detail/pages/movie-detail/movie-detail').then((m) => m.MovieDetail),
+  },
+  {
+    path: '**', // wildcard — qualquer rota que não exista
+    redirectTo: 'movies',
+  },
+];
