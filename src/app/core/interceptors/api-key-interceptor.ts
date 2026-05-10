153346f04607a4c74d97aeca6dea15b3ddab3512
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-10 17:26:46 +0100

    change error handler to interceptor

--- src/app/core/interceptors/api-key-interceptor.ts
+++ src/app/core/interceptors/api-key-interceptor.ts
@@ -1,9 +1,42 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { catchError, throwError } from 'rxjs';
 
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
   const clonedRequest = req.clone({
     params: req.params.set('api_key', environment.tmdbApiKey),
   });
-  return next(clonedRequest);
+  return next(clonedRequest).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let userMessage = 'An error has occurred';
+
+      switch (error.status) {
+        case 0:
+          userMessage = 'No internet connection. Check your internet connection.';
+          break;
+        case 401:
+          userMessage = 'Invalid API Key. Check your configs.';
+          break;
+        case 404:
+          userMessage = 'Resource not found.';
+          break;
+        case 429:
+          userMessage = 'To many requests. Try again later.';
+          break;
+        case 500:
+        case 502:
+        case 503:
+          userMessage = 'Server Error. Try again later.';
+          break;
+      }
+      if (!environment.production) {
+        console.error('[HTTP Error]', {
+          status: error.status,
+          url: error.url,
+          message: userMessage,
+        });
+      }
+      return throwError(() => new Error(userMessage));
+    }),
+  );
 };
--- src/app/core/services/tmdb.ts
+++ src/app/core/services/tmdb.ts
@@ -13,44 +13,20 @@ export class Tmdb {
 
   getPopularMovies(page: number = 1): Observable<MovieListResponse> {
     const params = new HttpParams().set('page', page);
-
-    return this.http
-      .get<MovieListResponse>(`${this.baseUrl}/movie/popular`, { params })
-      .pipe(catchError(this.handleError));
+    return this.http.get<MovieListResponse>(`${this.baseUrl}/movie/popular`, { params });
   }
 
   searchMovies(query: string, page: number = 1): Observable<MovieListResponse> {
     const params = new HttpParams().set('query', query).set('page', page);
 
-    return this.http
-      .get<MovieListResponse>(`${this.baseUrl}/search/movie`, { params })
-      .pipe(catchError(this.handleError));
+    return this.http.get<MovieListResponse>(`${this.baseUrl}/search/movie`, { params });
   }
 
   getMovieDetail(id: number): Observable<MovieDetail> {
-    return this.http
-      .get<MovieDetail>(`${this.baseUrl}/movie/${id}`)
-      .pipe(catchError(this.handleError));
+    return this.http.get<MovieDetail>(`${this.baseUrl}/movie/${id}`);
   }
 
   getMovieCredits(id: number): Observable<Credits> {
-    return this.http
-      .get<Credits>(`${this.baseUrl}/movie/${id}/credits`)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(error: any): Observable<never> {
-    let message = 'An error as occured.';
-
-    if (error.status == 401) {
-      message = 'Invalid or missign API Key.';
-    } else if (error.status == 404) {
-      message = 'Resource not found.';
-    } else if (error.status == 0) {
-      message = 'No connection. Please check your internet connection.';
-    }
-
-    console.error(`[TmdbService] ${message}`);
-    return throwError(() => new Error(message));
+    return this.http.get<Credits>(`${this.baseUrl}/movie/${id}/credits`);
   }
 }
