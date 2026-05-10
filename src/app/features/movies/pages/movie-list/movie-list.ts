0f1d5fbebdd3fc1145f621a3b520722a5fc9cd0a
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-10 16:32:40 +0100

    add movie list component

--- src/app/features/movies/pages/movie-list/movie-list.ts
+++ src/app/features/movies/pages/movie-list/movie-list.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MovieCard } from '../../components/movie-card/movie-card';
+import { SearchBar } from '../../components/search-bar/search-bar';
+import { LoadingSpinner } from '../../../../shared/components/loading-spinner/loading-spinner';
+import { catchError, of, Subject, switchMap, takeUntil } from 'rxjs';
+import { Tmdb } from '../../../../core/services/tmdb';
+import { Movie } from '../../../../core/models/movie.model';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [],
+  standalone: true,
+  imports: [MovieCard, SearchBar, LoadingSpinner],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.scss',
 })
-export class MovieList {}
+export class MovieList implements OnInit, OnDestroy {
+  movies: Movie[] = [];
+  isLoading = false;
+  errorMessage = '';
+  currentQuery = '';
+
+  private readonly searchQuery$ = new Subject<string>();
+  private readonly destroy$ = new Subject<void>();
+
+  constructor(private readonly tmdbService: Tmdb) {}
+
+  ngOnInit(): void {
+    this.searchQuery$
+      .pipe(
+        switchMap((query) => {
+          this.isLoading = true;
+          this.errorMessage = '';
+          this.currentQuery = query;
+
+          const request$ = query.trim()
+            ? this.tmdbService.searchMovies(query)
+            : this.tmdbService.getPopularMovies();
+
+          return request$.pipe(
+            catchError((err) => {
+              this.errorMessage = err.message;
+              return of({ results: [], page: 0, total_pages: 0, total_results: 0 });
+            }),
+          );
+        }),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((response) => {
+        this.movies = response.results;
+        this.isLoading = false;
+      });
+    this.searchQuery$.next('');
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
