153fc34e12b7e2218651ac038046b16ab451fcf0
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-10 16:33:35 +0100

    add movie card component

--- src/app/features/movies/components/movie-card/movie-card.ts
+++ src/app/features/movies/components/movie-card/movie-card.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Movie } from '../../../../core/models/movie.model';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-movie-card',
+  standalone: true,
   imports: [],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss',
 })
-export class MovieCard {}
+export class MovieCard {
+  readonly movie = input.required<Movie>();
+
+  readonly imageBaseUrl = environment.tmdbImageBaseUrl;
+
+  get postUrl(): string {
+    const path = this.movie().poster_path;
+    return path ? `${this.imageBaseUrl}${path}` : 'assets/placeholder.png';
+  }
+
+  get rating(): string {
+    return this.movie().vote_avarage.toFixed(1);
+  }
+
+  get releaseYear(): string {
+    return this.movie().release_date?.slice(0, 4) ?? 'N/A';
+  }
+}
