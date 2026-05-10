c141b89de33f4ae69cf848c8589d4de7b161a804
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-10 16:33:09 +0100

    add search bar component

--- src/app/features/movies/components/search-bar/search-bar.ts
+++ src/app/features/movies/components/search-bar/search-bar.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.scss',
 })
-export class SearchBar {}
+export class SearchBar implements OnInit, OnDestroy {
+  readonly searchChange = output<string>();
+  readonly searchControl = new FormControl('');
+  private readonly $destroy = new Subject<void>();
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged(), takeUntil(this.$destroy))
+      .subscribe((value) => {
+        this.searchChange.emit(value ?? '');
+      });
+  }
+
+  ngOnDestroy() {
+    this.$destroy.next();
+    this.$destroy.complete();
+  }
+}
