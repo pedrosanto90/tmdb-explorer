edcb2a42926dcdb8ccbc45a512fdf43c5730b909
Pedro Santo <pedro.bb.90@gmail.com>
2026-05-10 18:43:56 +0100

    add caching

--- src/app/core/services/tmdb.ts
+++ src/app/core/services/tmdb.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Credits, MovieDetail, MovieListResponse } from '../models/movie.model';
+import { shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,16 @@ import { Credits, MovieDetail, MovieListResponse } from '../models/movie.model';
 export class Tmdb {
   private readonly http = inject(HttpClient);
   private readonly baseUrl = environment.tmdbBaseUrl;
+  private popularMoviesCache$: Observable<MovieListResponse> | null = null;
 
   getPopularMovies(page: number = 1): Observable<MovieListResponse> {
-    const params = new HttpParams().set('page', page);
-    return this.http.get<MovieListResponse>(`${this.baseUrl}/movie/popular`, { params });
+    if (!this.popularMoviesCache$) {
+      const params = new HttpParams().set('page', page);
+      this.popularMoviesCache$ = this.http
+        .get<MovieListResponse>(`${this.baseUrl}/movie/popular`, { params })
+        .pipe(shareReplay(1));
+    }
+    return this.popularMoviesCache$;
   }
 
   searchMovies(query: string, page: number = 1): Observable<MovieListResponse> {
